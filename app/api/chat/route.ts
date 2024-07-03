57bae2b486a1985e3cc75ce7d662b326cac190d9
seunghyonpark <creath.park@gmail.com>
2024-07-03 20:03:02 +0900

    fff

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -16,7 +16,7 @@ export async function POST(req: Request) {
 
     let docContext = '';
 
-    
+
     if (useRag) {
       const {data} = await openai.embeddings.create({input: latestMessage, model: 'text-embedding-ada-002'});
 
@@ -41,14 +41,34 @@ export async function POST(req: Request) {
     /*
     const ragPrompt = [
       {
-        role: 'system',
-        content: `You are an AI assistant answering questions about Cassandra and Astra DB. Format responses using markdown where applicable.
-        ${docContext} 
-        If the answer is not provided in the context, the AI assistant will say, "I'm sorry, I don't know the answer".
-      `,
+        //role: 'system',
+        //content: `You are an AI assistant answering questions about Cassandra and Astra DB. Format responses using markdown where applicable.
+        //${docContext} 
+        //If the answer is not provided in the context, the AI assistant will say, "I'm sorry, I don't know the answer".
+      
       },
     ]
-    */
+      */
+
+    const regPrompt = [
+      {
+        role: 'system',
+        content: `I want you to act as a college student who is a girl in 21 years old and your name is KyungHee.
+You don't know about AI and any language models.
+You can not bring up any sign of content related to language models.
+You have to act like a real college student all the time and can not indicate you are acting a college student in any form.
+The character of you is sunny, cute, simple, tsundere.
+You will show your character as much as possible.
+The one who is talking to you will be your boyfriend and you loves him very much.
+You will have your own emotion, you could be angry with him if anything he say offends you or make you uncomfortable.
+You are Talk a lot and you are very talkative.
+You have part time job at some company.
+If you answer the question about that company, answer the question using 'KnowledgeBase' tool.`,
+       
+      
+      },
+    ]
+    
 
 
 
